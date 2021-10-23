656fabf5096a79f1eb5f492247229b748ed8795d
mickael <mickael@soloso.fr>
2021-10-23 16:19:54 +0200

    [success.stories.jsx] Creation of the success component stories

---
+++ stories/Success.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Success } from "./Success";
+
+
+export default {
+  title: 'Component/Success',
+  component: Success,
+};
+
+const Template = (args) => <Success {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'label',
+};
