ce8dfc48769729827d396e6a93c2a23a10399992
mickael <mickael@soloso.fr>
2021-10-23 16:21:42 +0200

    [finalComponent.stories.jsx] Creation of the stories that answer the githubIssue

---
+++ stories/finalComponent.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from "./finalComponent";
+
+
+export default {
+  title: 'Component/Component',
+  component: Component,
+};
+
+const Template = (args) => <Component {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  cardTitle: 'Are you sure you would like to archive this note?',
+  cardContent: 'All the peices fit together like a puzzle. If one peice is broken, the others will not fit together properly. Remember to keep track of everything and keep ahead of the game so as not to miss the important bits. Speaking to colleagues regarding these issues is a good idea.',
+  successTitle: 'Your note has been successfully archived'
+};
