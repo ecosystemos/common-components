38a2364ac4c919964d8be6462a66d1bea142d80e
mickael <mickael@soloso.fr>
2021-10-23 16:18:14 +0200

    [Title.jsx] Creation of the title component

---
+++ stories/Title.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Title = ({ title }) => {
+  return (
+      <Text> { title } </Text>
+  );
+};
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+
+const Text = styled.h1`
+  margin: 16px 0;
+  color: ${ props => props.theme.colors.textDarkGray };
+  font-family: ${ props => props.theme.fonts.roboto };
+  font-size: ${ props => props.theme.fontSizes.large };
+  font-weight: normal;
+`;
