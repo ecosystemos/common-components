3b7ca4498648b37762787333fa58585101520ccf
mickael <mickael@soloso.fr>
2021-10-23 16:22:54 +0200

    [preview.js] add the theme to the setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import Theme from "./theme";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,29 +13,8 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <Theme>
+    <Theme theme="theme">
       <Story />
     </Theme>
   ),
 ];
-
-
-
-const theme = {
-  colors: {
-    powderWhite: "#FFFDF9",
-    persianGreen: "#06B49A",
-    lightBlue: "#AFDBD2",
-    onyx: "#36313D"
-  },
-  fonts: ["sans-serif", "Roboto"],
-  fontSizes: {
-    small: "1em",
-    medium: "2em",
-    large: "3em"
-  }
-}
-
-const Theme = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
-);
