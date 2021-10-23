f4f67aab5ab7974f2cf229675c12badd45977e76
mickael <mickael@soloso.fr>
2021-10-23 16:20:18 +0200

    [Card.stories.jsx] Creation of the card component stories

---
+++ stories/Card.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from "./Card";
+
+
+export default {
+  title: 'Component/Card',
+  component: Card,
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'label',
+  content: 'label',
+  primaryFunction: undefined,
+  secondaryFunction: undefined,
+};
